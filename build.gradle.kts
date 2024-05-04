0dc5a115cfad9c46a13377b518443dbf0756c2cd
shatal2001 <shatal2001@yandex.ru>
2024-05-05 00:20:25 +0300

    security, swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+
+
 plugins {
     id("org.springframework.boot") version("2.7.8")
     java
+    id("io.freefair.lombok") version "8.6"
 }
 
 group = "ds.project"
@@ -15,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.7.8"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.7.18"))
 
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -23,6 +26,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(mapOf("group" to "org.junit.vintage", "module" to "junit-vintage-engine"))
     }
@@ -30,6 +35,7 @@ dependencies {
     //developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.named<Test>("test") {
